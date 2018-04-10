fd1c2e46b38f21ee2b192666b610fbd744eec38b
Alwayoung <34342700+Alwayoung@users.noreply.github.com>
2018-04-10 19:55:46 +0800

    Update CCMoveToActions.cs

--- Homework3/Priests and Devils/CCMoveToActions.cs
+++ Homework3/Priests and Devils/CCMoveToActions.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class CCMoveToActions : SSAction {
-
+public class CCMoveToActions : SSAction
+{
 	public Vector3 target;
 	public float speed;
 
-	public static CCMoveToActions GetSSAction(Vector3 target,float speed){
+	public static CCMoveToActions GetSSAction(Vector3 target,float speed)
+	{
 		CCMoveToActions action = ScriptableObject.CreateInstance<CCMoveToActions> ();
 		action.target = target;
 		action.speed = speed;
@@ -17,10 +18,11 @@ public class CCMoveToActions : SSAction {
 
 	public override void Start (){}
 
-
-	public override void Update(){
+	public override void Update()
+	{
 		this.transform.position = Vector3.MoveTowards (this.transform.position, target, speed * Time.deltaTime);
-		if (this.transform.position == target) {
+		if (this.transform.position == target)
+		{
 			this.destory = true;
 			this.callback.SSActionEvent (this);
 		}
